588dc76ac67d7654fa9ecf0a346584bbb589ed92
Adel Kedjour <akedjour@outlook.com>
2019-09-20 13:21:38 +0100

    Initial commit

---
+++ tests/ISoftBetTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace iSoftBetTes;
+
+use PHPUnit\Framework\TestCase;
+
+class ISoftBetTest extends TestCase
+{
+    public function test_if_phpunit_config_all_set_correct(): void
+    {
+        $this->assertTrue(true);
+    }
+}
